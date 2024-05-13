0b7bb90520c203a084135b669460e3573ddd1089
LARRYSALSA <75591878+LARRYICLOUD@users.noreply.github.com>
2024-05-12 20:53:53 -0500

    Agregar archivos de proyecto.

---
+++ Backend/Controllers/OperationController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]//Helper
+    [ApiController]
+    public class OperationController : ControllerBase
+    {
+        [HttpGet] //sirve cuando vas a obtener un recurso
+        public decimal Get(decimal a, decimal b) 
+        {
+            return a + b;
+        }
+
+        [HttpPost] //sirve para hacer cambio en nuestro servidor,insertando algo un fichero
+      //public decimal Add(decimal a, decimal b, Numbers numbers)
+
+            public decimal Add(Numbers numbers, [FromHeader] string Host,
+            [FromHeader(Name = "Content-Length")] string ContentLength,
+            [FromHeader(Name = "X-Some")] string Some)
+        {
+            Console.WriteLine(Host);
+            Console.WriteLine(ContentLength);
+            Console.WriteLine(Some);
+            return numbers.A - numbers.B;
+        }
+
+        [HttpPut] //sirve para actualizar propiedades
+
+        public decimal Edit(decimal a, decimal b)
+        {
+            return a * b;
+        }
+
+
+        [HttpDelete] // elimina registros
+
+        public decimal Delete(decimal a, decimal b)
+        {
+            return a / b;
+        }
+    }
+
+    public class Numbers
+    {
+        public decimal A { get; set; }
+        public decimal B { get; set; }
+    }
+
+
+
+
+}
+
